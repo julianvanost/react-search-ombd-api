41c496c5ed0545ddb99f70f841e68398ffc4a759
Julian <Van Ost>
2020-03-20 10:12:11 -0700

    Updates

---
+++ precache-manifest.22fc223d9ba45ddfdcbcb29854377608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970b8405cc6b86a6621d59673c32b14b",
+    "url": "/react-search-ombd-api/index.html"
+  },
+  {
+    "revision": "5f5afb7952981888836a",
+    "url": "/react-search-ombd-api/static/js/2.e62aa11e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-search-ombd-api/static/js/2.e62aa11e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621790335c9ae20ecdcf",
+    "url": "/react-search-ombd-api/static/js/main.b8b15720.chunk.js"
+  },
+  {
+    "revision": "c1af56a05b69c19febef",
+    "url": "/react-search-ombd-api/static/js/runtime-main.bd6c9aab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.f619496c01cefbdca2d7a60123c0cd86.js"
+  "/react-search-ombd-api/precache-manifest.22fc223d9ba45ddfdcbcb29854377608.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-search-ombd-api/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
